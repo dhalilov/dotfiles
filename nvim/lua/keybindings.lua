50a362a5448aa1ef46fce18f5dae78c1040ffb26
Dario Halilovic <dario.halilovic@epfl.ch>
2024-07-05 17:47:40 +0200

    Add keybindings for moving ranges around in visual mode

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -31,3 +31,7 @@ vim.keymap.set({"n", "v"}, "<Leader>y", "\"+y")
 
 -- Paste from clipboard
 vim.keymap.set({"n", "v"}, "<Leader>p", "\"+p")
+
+-- Move around ranges in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
