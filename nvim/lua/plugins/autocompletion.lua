ab2676d62b265acb9dfb1d21dae2b51e1f974f1a
Dario Halilovic <dario.halilovic@epfl.ch>
2024-11-11 18:45:59 +0100

    Add snippet engine

--- nvim/lua/plugins/autocompletion.lua
+++ nvim/lua/plugins/autocompletion.lua
@@ -8,10 +8,39 @@ return {
 		-- Add LSP and path completion sources
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-path",
+
+		-- Add a snippet engine and its associated `nvim-cmp` source
+		{
+			"L3MON4D3/LuaSnip",
+			build = (function()
+				if vim.fn.has "win32" == 1 or vim.fn.executable "make" == 0 then
+					return
+				end
+				return "make install_jsregexp"
+			end)(),
+			dependencies = {
+				-- `friendly-snippets` contains a variety of premade snippets.
+				{
+					"rafamadriz/friendly-snippets",
+					config = function()
+						require("luasnip.loaders.from_vscode").lazy_load()
+					end,
+				},
+			},
+		},
+		"saadparwaiz1/cmp_luasnip",
 	},
 	config = function()
 		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+		luasnip.config.setup {}
+
 		cmp.setup {
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
 			completion = { completeopt = "menu,menuone,noinsert" },
 			mapping = cmp.mapping.preset.insert {
 				-- Select the [n]ext item
@@ -30,6 +59,20 @@ return {
 
 				-- Manually trigger a completion from nvim-cmp
 				["<C-Space>"] = cmp.mapping.complete {},
+
+				-- Goto next snippet location
+				["<C-l>"] = cmp.mapping(function()
+					if luasnip.expand_or_locally_jumpable() then
+						luasnip.expand_or_jump()
+					end
+				end, { "i", "s" }),
+
+				-- Goto previous snippet location
+				["<C-h>"] = cmp.mapping(function()
+					if luasnip.locally_jumpable(-1) then
+						luasnip.jump(-1)
+					end
+				end, { "i", "s" }),
 			},
 			sources = {
 				{
@@ -38,6 +81,7 @@ return {
 					group_index = 0,
 				},
 				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
 				{ name = "path" },
 			},
 		}
