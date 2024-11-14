4761ddb6c7a05e55cadcceae2a4da576432e3705
Dario Halilovic <dario.halilovic@epfl.ch>
2024-11-14 17:54:52 +0100

    Add timeout to autoformat

--- nvim/lua/plugins/autoformat.lua
+++ nvim/lua/plugins/autoformat.lua
@@ -14,7 +14,8 @@ return {
 		}
 	},
 	opts = {
-		notify_on_error = false,
+		notify_on_error = true,
+		format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
 		formatters_by_ft = {
 			lua = { "stylua" },
 			python = { "black" }
