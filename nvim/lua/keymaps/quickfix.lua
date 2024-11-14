07006176d3162f120e5a1f4779861d7a58be0ecc
Dario Halilovic <dario.halilovic@epfl.ch>
2024-11-14 20:46:18 +0100

    Fix comment for all diagnostics keymap

--- nvim/lua/keymaps/quickfix.lua
+++ nvim/lua/keymaps/quickfix.lua
@@ -1,6 +1,6 @@
 -- Keymaps for the quickfix list
 
--- Add errors to the quickfix list
+-- Add all diagnostics to the quickfix list
 vim.keymap.set("n", "<leader>aa", vim.diagnostic.setqflist, { desc = "[A]dd [a]ll diagnostics to the quickfix list" })
 
 -- Add errors to the quickfix list
