3567398434bc1e282b6c9b39c9674d979cb20cb3
Dario Halilovic <dario.halilovic@epfl.ch>
2024-11-20 01:29:28 +0100

    Avoid inserting parentheses after LSP function completion

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -3,12 +3,5 @@
 return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
-	config = function()
-		require("nvim-autopairs").setup {}
-
-		-- Automatically add a ( after completing a function
-		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-		local cmp = require("cmp")
-		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-	end
+	opts = {}
 }
